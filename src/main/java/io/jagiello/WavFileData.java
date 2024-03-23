1d51013e16c305d44ffbee3bc749e6f81a435ebf
Miroslaw Jagiello <mirek.jagiello@gmail.com>
2024-03-23 08:22:58 +0100

    D023/100 refactor

--- src/main/java/io/jagiello/WavFileData.java
+++ src/main/java/io/jagiello/WavFileData.java
@@ -1,14 +1,51 @@
 package io.jagiello;
 
 import lombok.Builder;
+import lombok.Getter;
 
-@Builder
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.UnsupportedAudioFileException;
+import java.io.File;
+import java.io.IOException;
+
+@Getter
 class WavFileData {
-    private long audioFileLength;
-    private int frameSize;
-    private float frameRate;
-    private long numFrames; // audioFileLength / frameSize;
+    final private long audioFileLength;
+    final private int frameSize;
+    final private float frameRate;
+    final private long numFrames;
 
-    private float[] audioSamples = new float[(int)numFrames];
-    private byte[] audioBytes = new byte[(int)audioFileLength];
+    final private float[] audioSamples;
+    final private byte[] audioBytes;
+
+    WavFileData(File audioFile) throws UnsupportedAudioFileException, IOException {
+        AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(audioFile);
+        AudioFormat format = audioInputStream.getFormat();
+        this.audioFileLength = audioFile.length();
+        this.frameSize = format.getFrameSize();
+        this.frameRate = format.getFrameRate();
+        this.numFrames = audioFileLength / frameSize;
+
+        this.audioSamples = new float[(int)numFrames];
+        this.audioBytes = new byte[(int)audioFileLength];
+
+        // Read the audio bytes and convert them to samples
+        audioInputStream.read(audioBytes);
+
+        float maxAudioSample = 0f;
+        float minAudioSample = 0f;
+        // Assuming 16-bit samples, mono
+        for (int i = 0, sampleIndex = 0; i < audioBytes.length && sampleIndex < audioSamples.length; i += 2, sampleIndex++) {
+            int audioSample = (audioBytes[i + 1] << 8) | (audioBytes[i] & 0xFF);
+            audioSamples[sampleIndex] = audioSample / 32768.0F; // Normalize
+            if(audioSamples[sampleIndex] > maxAudioSample) {
+                maxAudioSample = audioSamples[sampleIndex];
+            }
+            if(audioSamples[sampleIndex] < minAudioSample) {
+                minAudioSample = audioSamples[sampleIndex];
+            }
+        }
+    }
 }
--- src/main/java/io/jagiello/WavFileProcessor.java
+++ src/main/java/io/jagiello/WavFileProcessor.java
@@ -3,9 +3,6 @@ package io.jagiello;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 
-import javax.sound.sampled.AudioFormat;
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.UnsupportedAudioFileException;
 import java.io.File;
 import java.io.IOException;
@@ -17,51 +14,21 @@ class WavFileProcessor {
     static void process(File audioFile, Canvas wavCanvas,
                         AtomicReference<Double> tau, Canvas soundLevelCanvas)
             throws UnsupportedAudioFileException, IOException {
-        AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(audioFile);
-        AudioFormat format = audioInputStream.getFormat();
-        long audioFileLength = audioFile.length();
-        int frameSize = format.getFrameSize();
-        float frameRate = format.getFrameRate();
-        long numFrames = audioFileLength / frameSize;
 
-        float[] audioSamples = new float[(int)numFrames];
-        byte[] audioBytes = new byte[(int)audioFileLength];
-
-        WavFileData wavFileData = WavFileData.builder()
-                .audioFileLength(audioFile.length())
-                .frameSize(format.getFrameSize())
-                .frameRate(format.getFrameRate())
-                .numFrames(audioFileLength / frameSize)
-                .audioSamples(new float[(int)numFrames])
-                .audioBytes(new byte[(int)audioFileLength])
-                .build();
-
-        // Read the audio bytes and convert them to samples
-        audioInputStream.read(audioBytes);
-
-        float maxAudioSample = 0f;
-        float minAudioSample = 0f;
-        // Assuming 16-bit samples, mono
-        for (int i = 0, sampleIndex = 0; i < audioBytes.length && sampleIndex < audioSamples.length; i += 2, sampleIndex++) {
-            int audioSample = (audioBytes[i + 1] << 8) | (audioBytes[i] & 0xFF);
-            audioSamples[sampleIndex] = audioSample / 32768.0F; // Normalize
-            if(audioSamples[sampleIndex] > maxAudioSample) {
-                maxAudioSample = audioSamples[sampleIndex];
-            }
-            if(audioSamples[sampleIndex] < minAudioSample) {
-                minAudioSample = audioSamples[sampleIndex];
-            }
-        }
+        WavFileData wavFileData = new WavFileData(audioFile);
 
         GraphicsContext wavCanvasGc = wavCanvas.getGraphicsContext2D();
         wavCanvasGc.clearRect(0, 0, wavCanvas.getWidth(), wavCanvas.getHeight());
-        for (int i = 0; i < audioSamples.length - 1; i++) {
-            wavCanvasGc.fillRect(i * audioSamples.length, 300 - audioSamples[i] / 128, audioSamples.length, 300);
+        for (int i = 0; i < wavFileData.getAudioSamples().length - 1; i++) {
+            wavCanvasGc.fillRect(i * wavFileData.getAudioSamples().length,
+                    300 - wavFileData.getAudioSamples()[i] / 128, wavFileData.getAudioSamples().length, 300);
         }
 
-        drawWave(wavCanvasGc, audioSamples);
+        drawWave(wavCanvasGc, wavFileData.getAudioSamples());
 
-        double[] soundLevel = DecibelCalculator.LdB(ExponentialAveragingCalculator.calculate(audioSamples, tau.get().floatValue(), frameRate));
+        double[] soundLevel = DecibelCalculator.LdB(
+                ExponentialAveragingCalculator.calculate(
+                        wavFileData.getAudioSamples(), tau.get().floatValue(), wavFileData.getFrameRate()));
         GraphicsContext soundLevelCanvasGc = soundLevelCanvas.getGraphicsContext2D();
         soundLevelCanvasGc.clearRect(0, 0, soundLevelCanvas.getWidth(), soundLevelCanvas.getHeight());
         drawWave(soundLevelCanvasGc, soundLevel);
