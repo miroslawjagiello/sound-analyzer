711b842cc687a5dd057d37bfcc59735d853bfdba
Miroslaw Jagiello <mirek.jagiello@gmail.com>
2024-03-31 07:12:14 +0200

    D031/100 refactor

--- src/main/java/io/jagiello/FileInfoLabels.java
+++ src/main/java/io/jagiello/FileInfoLabels.java
@@ -6,4 +6,16 @@ class FileInfoLabels {
     private Label fileName = new Label("");
     private Label samplingRate = new Label("");
     private Label sampleSizeInBits = new Label("");
+
+    public void setFileName(String fileName) {
+        this.fileName.setText("File name: " + fileName);
+    }
+
+    public void setSamplingRate(String samplingRate) {
+        this.samplingRate.setText("Sampling rate: " + samplingRate + " Hz");
+    }
+
+    public void setSampleSizeInBits(String sampleSizeInBits) {
+        this.sampleSizeInBits.setText(sampleSizeInBits + " bit");
+    };
 }
