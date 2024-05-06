e9e7d459357cec4857d50753bafa2c2631ac072e
Miroslaw Jagiello <mirek.jagiello@gmail.com>
2024-05-06 15:34:19 +0200

    D067/100 refactor

--- src/main/java/io/jagiello/SoundLevelVisualizer.java
+++ src/main/java/io/jagiello/SoundLevelVisualizer.java
@@ -9,16 +9,16 @@ class SoundLevelVisualizer {
     private static final double HEIGHT = 400;
     private static final double CENTER_Y = HEIGHT / 2;
 
-    static void drawSoundLevel(GraphicsContext gc, double[] audioSamples) {
+    static void drawSoundLevel(GraphicsContext gc, double[] soundLevels) {
         double width = gc.getCanvas().getWidth();
         double height = gc.getCanvas().getHeight();
         double lastX = 0, lastY = height / 2;
 
         gc.setStroke(Color.BLUE);
-        for (int i = 0; i < audioSamples.length; i++) {
-            double x = (double) i / audioSamples.length * width;
-            //double y = height / 2 - audioSamples[i] * 2;
-            double y = height / 4 - audioSamples[i] * 2;
+        for (int i = 0; i < soundLevels.length; i++) {
+            double x = (double) i / soundLevels.length * width;
+            //double y = height / 2 - soundLevels[i] * 2;
+            double y = height / 4 - soundLevels[i] * 2;
             gc.strokeLine(lastX, lastY, x, y);
             lastX = x;
             lastY = y;
